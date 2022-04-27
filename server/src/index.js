e8330e21860a7e029d76a3884b5a22cd2531f180
nguyenfamj <nguyenfamj1409@gmail.com>
2022-04-28 02:14:00 +0300

    Continue fixing cors

--- server/src/index.js
+++ server/src/index.js
@@ -27,7 +27,10 @@ connectDB(mongoURI)
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Credentials', true);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      );
       next();
     });
     app.use(express.json());
