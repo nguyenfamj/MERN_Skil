10684eec818e3cf2b0f09b35aa0638e74834e0c0
nguyenfamj <nguyenfamj1409@gmail.com>
2022-04-28 02:24:18 +0300

    Fixing

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,7 @@ connectDB(mongoURI)
     // Setup middlewares
     app.use(cors());
     app.use(function (req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
       res.header('Access-Control-Allow-Credentials', true);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header(
